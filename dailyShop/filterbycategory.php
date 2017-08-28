6a689f533f6ed18823a2c2f73431098e33e10ae1
shubhangis3116 <shubhangisrivastava@cedcoss.com>
2017-08-28 17:07:10 +0530

    category

--- dailyShop/filterbycategory.php
+++ dailyShop/filterbycategory.php
@@ -6,7 +6,7 @@
               global $conn,$stmt;
               $cat=$_GET['cat'];
               $stmt=$conn->prepare("SELECT COUNT(*) FROM newproductlist WHERE category=?");
-                $stmt->bind_param("s",$cat)
+                $stmt->bind_param("s",$cat);
                 $stmt->bind_result($num);
                 $limit=9;
                 $offset=0;
@@ -339,126 +339,7 @@
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-3 col-sm-4 col-md-pull-9">
-            <aside class="aa-sidebar">
-              <!-- single sidebar -->
-              <div class="aa-sidebar-widget">
-                <h3>Category</h3>
-                <ul class="aa-catg-nav">
-
-                  <li><a href="product.php">Men</a></li>
-                  <li><a href="">Women</a></li>
-                 
-                  <li><a href="">Electornics</a></li>
-                  <li><a href="">Sports</a></li>
-                </ul>
-              </div>
-              <!-- single sidebar -->
-              <div class="aa-sidebar-widget">
-                <h3>Tags</h3>
-                <div class="tag-cloud">
-                  <a href="#">Fashion</a>
-                  <a href="#">Ecommerce</a>
-                  <a href="#">Shop</a>
-                  <a href="#">Hand Bag</a>
-                  <a href="#">Laptop</a>
-                  <a href="#">Head Phone</a>
-                  <a href="#">Pen Drive</a>
-                </div>
-              </div>
-              <!-- single sidebar -->
-              <div class="aa-sidebar-widget">
-                <h3>Shop By Price</h3>              
-                <!-- price range -->
-                <div class="aa-sidebar-price-range">
-                 <form action="">
-                    <div id="skipstep" class="noUi-target noUi-ltr noUi-horizontal noUi-background">
-                    </div>
-                    <span id="skip-value-lower" class="example-val">30.00</span>
-                   <span id="skip-value-upper" class="example-val">100.00</span>
-                   <button class="aa-filter-btn" type="submit">Filter</button>
-                 </form>
-                </div>              
-
-              </div>
-              <!-- single sidebar -->
-              <div class="aa-sidebar-widget">
-                <h3>Shop By Color</h3>
-                <div class="aa-color-tag">
-                  <a class="aa-color-green" href="#"></a>
-                  <a class="aa-color-yellow" href="#"></a>
-                  <a class="aa-color-pink" href="#"></a>
-                  <a class="aa-color-purple" href="#"></a>
-                  <a class="aa-color-blue" href="#"></a>
-                  <a class="aa-color-orange" href="#"></a>
-                  <a class="aa-color-gray" href="#"></a>
-                  <a class="aa-color-black" href="#"></a>
-                  <a class="aa-color-white" href="#"></a>
-                  <a class="aa-color-cyan" href="#"></a>
-                  <a class="aa-color-olive" href="#"></a>
-                  <a class="aa-color-orchid" href="#"></a>
-                </div>                            
-              </div>
-              <!-- single sidebar -->
-              <div class="aa-sidebar-widget">
-                <h3>Recently Views</h3>
-                <div class="aa-recently-views">
-                  <ul>
-                    <li>
-                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
-                      <div class="aa-cartbox-info">
-                        <h4><a href="#">Product Name</a></h4>
-                        <p>1 x $250</p>
-                      </div>                    
-                    </li>
-                    <li>
-                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-1.jpg"></a>
-                      <div class="aa-cartbox-info">
-                        <h4><a href="#">Product Name</a></h4>
-                        <p>1 x $250</p>
-                      </div>                    
-                    </li>
-                     <li>
-                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
-                      <div class="aa-cartbox-info">
-                        <h4><a href="#">Product Name</a></h4>
-                        <p>1 x $250</p>
-                      </div>                    
-                    </li>                                      
-                  </ul>
-                </div>                            
-              </div>
-              <!-- single sidebar -->
-              <div class="aa-sidebar-widget">
-                <h3>Top Rated Products</h3>
-                <div class="aa-recently-views">
-                  <ul>
-                    <li>
-                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
-                      <div class="aa-cartbox-info">
-                        <h4><a href="#">Product Name</a></h4>
-                        <p>1 x $250</p>
-                      </div>                    
-                    </li>
-                    <li>
-                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-1.jpg"></a>
-                      <div class="aa-cartbox-info">
-                        <h4><a href="#">Product Name</a></h4>
-                        <p>1 x $250</p>
-                      </div>                    
-                    </li>
-                     <li>
-                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
-                      <div class="aa-cartbox-info">
-                        <h4><a href="#">Product Name</a></h4>
-                        <p>1 x $250</p>
-                      </div>                    
-                    </li>                                      
-                  </ul>
-                </div>                            
-              </div>
-            </aside>
-          </div>
+         <?php include('sidebar.php'); ?>
          
         </div>
       </div>
--- dailyShop/sidebar.php
+++ dailyShop/sidebar.php
@@ -1,76 +1,132 @@
- <!-- Start slider -->
-      <section id="aa-slider">
-        <div class="aa-slider-area">
-          <div id="sequence" class="seq">
-            <div class="seq-screen">
-              <ul class="seq-canvas">
-                <!-- single slide item -->
-                <li>
-                  <div class="seq-model">
-                    <img data-seq src="img/slider/1.jpg" alt="Men slide img" />
-                  </div>
-                  <div class="seq-title">
-                   <span data-seq>Save Up to 75% Off</span>                
-                    <h2 data-seq>Men Collection</h2>                
-                    <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
-                    <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
-                  </div>
-                </li>
-                <!-- single slide item -->
-                <li>
-                  <div class="seq-model">
-                    <img data-seq src="img/slider/2.jpg" alt="Wristwatch slide img" />
-                  </div>
-                  <div class="seq-title">
-                    <span data-seq>Save Up to 40% Off</span>                
-                    <h2 data-seq>Wristwatch Collection</h2>                
-                    <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
-                    <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
-                  </div>
-                </li>
-                <!-- single slide item -->
-                <li>
-                  <div class="seq-model">
-                    <img data-seq src="img/slider/3.jpg" alt="Women Jeans slide img" />
-                  </div>
-                  <div class="seq-title">
-                    <span data-seq>Save Up to 75% Off</span>                
-                    <h2 data-seq>Jeans Collection</h2>                
-                    <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
-                    <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
-                  </div>
-                </li>
-                <!-- single slide item -->           
-                <li>
-                  <div class="seq-model">
-                    <img data-seq src="img/slider/4.jpg" alt="Shoes slide img" />
-                  </div>
-                  <div class="seq-title">
-                    <span data-seq>Save Up to 75% Off</span>                
-                    <h2 data-seq>Exclusive Shoes</h2>                
-                    <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
-                    <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
-                  </div>
-                </li>
-                <!-- single slide item -->  
-                 <li>
-                  <div class="seq-model">
-                    <img data-seq src="img/slider/5.jpg" alt="Male Female slide img" />
-                  </div>
-                  <div class="seq-title">
-                    <span data-seq>Save Up to 50% Off</span>                
-                    <h2 data-seq>Best Collection</h2>                
-                    <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
-                    <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
-                  </div>
-                </li>                   
-              </ul>
-            </div>
-            <!-- slider navigation btn -->
-            <fieldset class="seq-nav" aria-controls="sequence" aria-label="Slider buttons">
-              <a type="button" class="seq-prev" aria-label="Previous"><span class="fa fa-angle-left"></span></a>
-              <a type="button" class="seq-next" aria-label="Next"><span class="fa fa-angle-right"></span></a>
-            </fieldset>
+<?php include('config.php'); 
+$catarray=array();
+$stmt=$conn->prepare("SELECT name FROM newcategory WHERE parentid=?");
+$select=0;
+$stmt->bind_param("i",$select);
+$stmt->bind_result($new);
+        $stmt->execute();
+        while($stmt->fetch())
+        {
+          array_push($catarray,array("name"=>$new));
+        }
+
+        ?>
+<div class="col-lg-3 col-md-3 col-sm-4 col-md-pull-9">
+            <aside class="aa-sidebar">
+              <!-- single sidebar -->
+              <div class="aa-sidebar-widget">
+                <h3>Category</h3>
+                <ul class="aa-catg-nav">
+                <?php foreach($catarray as $key => $value): ?>
+
+                  <li><a href="filterbycategory.php?cat=<?php echo $value['name']; ?>&pageid=1"><?php echo $value['name']; ?></a></li>
+                  <?php endforeach; ?>
+                </ul>
+              </div>
+              <!-- single sidebar -->
+              <div class="aa-sidebar-widget">
+                <h3>Tags</h3>
+                <div class="tag-cloud">
+                  <a href="#">Fashion</a>
+                  <a href="#">Ecommerce</a>
+                  <a href="#">Shop</a>
+                  <a href="#">Hand Bag</a>
+                  <a href="#">Laptop</a>
+                  <a href="#">Head Phone</a>
+                  <a href="#">Pen Drive</a>
+                </div>
+              </div>
+              <!-- single sidebar -->
+              <div class="aa-sidebar-widget">
+                <h3>Shop By Price</h3>              
+                <!-- price range -->
+                <div class="aa-sidebar-price-range">
+                 <form action="">
+                    <div id="skipstep" class="noUi-target noUi-ltr noUi-horizontal noUi-background">
+                    </div>
+                    <span id="skip-value-lower" class="example-val">30.00</span>
+                   <span id="skip-value-upper" class="example-val">100.00</span>
+                   <button class="aa-filter-btn" type="submit">Filter</button>
+                 </form>
+                </div>              
+
+              </div>
+              <!-- single sidebar -->
+              <div class="aa-sidebar-widget">
+                <h3>Shop By Color</h3>
+                <div class="aa-color-tag">
+                  <a class="aa-color-green" href="#"></a>
+                  <a class="aa-color-yellow" href="#"></a>
+                  <a class="aa-color-pink" href="#"></a>
+                  <a class="aa-color-purple" href="#"></a>
+                  <a class="aa-color-blue" href="#"></a>
+                  <a class="aa-color-orange" href="#"></a>
+                  <a class="aa-color-gray" href="#"></a>
+                  <a class="aa-color-black" href="#"></a>
+                  <a class="aa-color-white" href="#"></a>
+                  <a class="aa-color-cyan" href="#"></a>
+                  <a class="aa-color-olive" href="#"></a>
+                  <a class="aa-color-orchid" href="#"></a>
+                </div>                            
+              </div>
+              <!-- single sidebar -->
+              <div class="aa-sidebar-widget">
+                <h3>Recently Views</h3>
+                <div class="aa-recently-views">
+                  <ul>
+                    <li>
+                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">Product Name</a></h4>
+                        <p>1 x $250</p>
+                      </div>                    
+                    </li>
+                    <li>
+                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-1.jpg"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">Product Name</a></h4>
+                        <p>1 x $250</p>
+                      </div>                    
+                    </li>
+                     <li>
+                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">Product Name</a></h4>
+                        <p>1 x $250</p>
+                      </div>                    
+                    </li>                                      
+                  </ul>
+                </div>                            
+              </div>
+              <!-- single sidebar -->
+              <div class="aa-sidebar-widget">
+                <h3>Top Rated Products</h3>
+                <div class="aa-recently-views">
+                  <ul>
+                    <li>
+                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">Product Name</a></h4>
+                        <p>1 x $250</p>
+                      </div>                    
+                    </li>
+                    <li>
+                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-1.jpg"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">Product Name</a></h4>
+                        <p>1 x $250</p>
+                      </div>                    
+                    </li>
+                     <li>
+                      <a href="#" class="aa-cartbox-img"><img alt="img" src="img/woman-small-2.jpg"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">Product Name</a></h4>
+                        <p>1 x $250</p>
+                      </div>                    
+                    </li>                                      
+                  </ul>
+                </div>                            
+              </div>
+            </aside>
           </div>
-        </div>
-      </section>
+         
